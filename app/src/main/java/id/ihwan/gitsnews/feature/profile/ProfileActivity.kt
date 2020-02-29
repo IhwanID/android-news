82655e69e719917df9e3de638c73e69a7f11e711
IhwanID <mynameisihwan@gmail.com>
2020-02-29 22:36:45 +0700

    update function

--- app/src/main/java/id/ihwan/gitsnews/feature/profile/ProfileActivity.kt
+++ app/src/main/java/id/ihwan/gitsnews/feature/profile/ProfileActivity.kt
@@ -1,5 +1,6 @@
 package id.ihwan.gitsnews.feature.profile
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.activity.viewModels
@@ -8,6 +9,7 @@ import androidx.lifecycle.ViewModelProvider
 import id.ihwan.gitsnews.R
 import id.ihwan.gitsnews.core.platform.BaseActivity
 import id.ihwan.gitsnews.databinding.ActivityProfileBinding
+import id.ihwan.gitsnews.feature.home.view.WebViewActivity
 import javax.inject.Inject
 
 class ProfileActivity : BaseActivity<ActivityProfileBinding>(R.layout.activity_profile) {
@@ -23,5 +25,14 @@ class ProfileActivity : BaseActivity<ActivityProfileBinding>(R.layout.activity_p
         viewModel.getProfile().observe(this, Observer {
            binding.profile = it
         })
+    }
+
+    fun goToDetail(url: String?, title: String?) {
+
+        val i = Intent(this, WebViewActivity::class.java).apply {
+            putExtra("url", url)
+            putExtra("title", title)
+        }
+        startActivity(i)
     }
 }
